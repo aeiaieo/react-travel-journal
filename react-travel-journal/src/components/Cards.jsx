1f8bf23b3a2a2d29ad006410d4c74ff75d34130b
aeiaieo <alexislaineboldt@gmail.com>
2023-10-02 12:02:45 -0700

    Project setup
    React with Vite implemented and their files adjusted, and default text/styling removed. Page header coded and styled. Components folder added. Image/props implementation began.

---
+++ react-travel-journal/src/components/Cards.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Cards(props){
+    return (
+        <div>
+            <img src={props.img} alt="Seoul" />
+        </div>
+    )
+}
---
+++ react-travel-journal/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// import ReactDOM from 'react-dom/client'
+
+export default function Header(){
+    return (
+        <div className="headerTitle">
+            <i className="fa-solid fa-map-pin headBar"></i>
+            <h1 className="headBar" >aeiaieo's travel journal</h1>
+        </div>
+    )
+}
